e04c73135b43d23e15ada0f951ae944a8dc7887c
Vladyslav_Mazarchuk <ipz222_mva@student.ztu.edu.ua>
2024-05-30 02:51:09 +0300

    Додано абстрактний клас Subscription для визначення загальних властивостей підписок

---
+++ Lab2/FactoryMethod/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public abstract class Subscription
+    {
+        public abstract decimal MonthlyFee { get; }
+        public abstract int MinimumPeriod { get; }
+        public abstract List<string> Channels { get; }
+        public abstract List<string> Features { get; }
+        public abstract void ShowDetails();
+    }
+}
