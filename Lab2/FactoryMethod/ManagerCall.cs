15de5aadd6f3326a528da90c21e0b874b0af8470
Vladyslav_Mazarchuk <ipz222_mva@student.ztu.edu.ua>
2024-05-30 02:49:07 +0300

    одано клас ManagerCall для створення підписки через дзвінок менеджера

---
+++ Lab2/FactoryMethod/ManagerCall.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class ManagerCall : SubscriptionCreator
+    {
+        public override Subscription CreateDomesticSubscription()
+        {
+            return new DomesticSubscription();
+        }
+
+        public override Subscription CreateEducationalSubscription()
+        {
+            return new EducationalSubscription();
+        }
+
+        public override Subscription CreatePremiumSubscription()
+        {
+            return new PremiumSubscription();
+        }
+
+        public override string GetCreatorType()
+        {
+            return "Manager Call";
+        }
+    }
+}
